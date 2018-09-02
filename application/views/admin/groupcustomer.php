6e5545f1a89f27344807bd724c9c55cc93c929c4
riyajuddin <mdriya93@gmail.com>
2018-09-02 23:49:04 +0530

    loan secton
     loan master and group master start creating

---
+++ application/views/admin/groupcustomer.php
@@ -0,0 +1,339 @@
+	<?php $this->load->view('global/header');?>
+    <?php $this->load->view('global/side_nav');?>
+    <main class="app-content">
+      <div class="app-title">
+        <div>
+        <ul class="app-breadcrumb breadcrumb">
+          <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
+          <li class="breadcrumb-item">Loan Master</li>
+        </ul>
+		</div>
+		<p class="bs-component">	
+            <a onclick="addLoanmasterform()" style="color:#fff" class="btn btn-sm btn-success">New</a>
+            <button class="btn btn-sm btn-danger" type="button" onclick="deleteLoanmaster()">Delete</button>
+        </p>
+      </div>
+      
+      <div class="row" id="MasLoanmasterformColap">
+      </div>
+     
+      <div class="row">
+        <div class="col-md-12">
+          <div class="tile">
+          <div class="row"> 
+              	<div class="col-md-12">
+                	<div id="loanmaster_table" class="tile-body"></div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+	  <?php $this->load->view('global/footer');?>
+
+    <script type="text/javascript">
+   
+    	
+   
+    function loadLoanmaster()
+    { 
+      var url = "<?php echo site_url('index.php/data_controller/loadLoanmaster'); ?>"; 
+      StartInsideLoading();
+      $.ajax({
+        type: "post",
+        url: url,
+        cache: false,   
+        dataType: 'json', 
+        success: function(response){ 
+        try{  
+          if (response.success)
+             { 
+            $('#loanmaster_table').html(response.html);
+              $('#loanmaster').DataTable();
+              
+             } else
+             { 
+                 SetWarningMessageBox('warning', response.msg);
+                
+             }
+         StopInsideLoading();
+         
+         }catch(e) {  
+            SetWarningMessageBox('warning', e);
+            StopInsideLoading();
+          } 
+        },
+        error: function(){      
+          SetWarningMessageBox('warning', 'Error while request..');
+          StopInsideLoading();
+        }
+       });
+    }
+    
+      loadLoanmaster();
+    function addLoanmaster(){  
+    	if ($('#loanmaster_name').val().trim() == '') { 
+            SetWarningMessageBox('warning', 'Loan name is mandatory !');
+            $('#loanmaster_name').focus();
+            return;
+        }
+		if ($('#loanmaster_pc').val().trim() == '') { 
+            SetWarningMessageBox('warning', 'Loan PC is mandatory !');
+            $('#loanmaster_pc').focus();
+            return;
+        }
+		if ($('#loanmaster_pc_type').val().trim() == '') { 
+            SetWarningMessageBox('warning', 'Loan PC type is mandatory !');
+            $('#loanmaster_pc_type').focus();
+            return;
+        }
+		
+			if ($('#loanmaster_tenure_type').val().trim() == '') { 
+            SetWarningMessageBox('warning', 'Loan Tenure type is mandatory !');
+            $('#loanmaster_tenure_type').focus();
+            return;
+        }
+			if ($('#loanmaster_tenure_min').val().trim() == '') { 
+            SetWarningMessageBox('warning', 'Loan tenure Min amount is mandatory !');
+            $('#loanmaster_tenure_min').focus();
+            return;
+        }
+			if ($('#loanmaster_tenure_max').val().trim() == '') { 
+            SetWarningMessageBox('warning', 'Loan tenure Max amount is mandatory !');
+            $('#loanmaster_tenure_max').focus();
+            return;
+        }
+			if ($('#loanmaster_min_amount').val().trim() == '') { 
+            SetWarningMessageBox('warning', 'Loan Min amount is mandatory !');
+            $('#loanmaster_min_amount').focus();
+            return;
+        }
+			if ($('#loanmaster_max_amount').val().trim() == '') { 
+            SetWarningMessageBox('warning', 'Loan Max amount is mandatory !');
+            $('#loanmaster_max_amount').focus();
+            return;
+        }
+       
+        
+        var formData = $('form#MasLoanmasterForms').serializeObject();
+        var dataString = JSON.stringify(formData);
+        var url = '<?php echo base_url();?>index.php/data_controller/addBranch';
+     StartInsideLoading();
+     $.ajax({
+      type: "post",
+      url: url,
+      cache: false,    
+      data: dataString,
+      dataType: 'json',
+      success: function(response){   
+      try{ 
+         if (response.success)
+             { 
+           SetSucessMessageBox('Success', response.msg);
+           $('#MasBranchformColap').empty(); 
+           loadBranch();
+           $('#Branch').DataTable();
+             } else
+             { 
+                 SetWarningMessageBox('warning', response.msg);
+             }
+      StopInsideLoading();
+      }catch(e) {  
+        SetWarningMessageBox('warning', e);
+        StopInsideLoading();
+      }  
+      },
+      error: function(){      
+        SetWarningMessageBox('warning', 'Error while request..');
+        StopInsideLoading();
+      }
+     });
+    }
+
+
+    function addBranchform(){ 
+    	var url = '<?php echo base_url();?>index.php/data_controller/AddBranchform';
+    	StartInsideLoading();
+    	$.ajax({
+    		  type: "post",
+    		  url: url,
+    		  cache: false,
+    		  dataType: 'json',
+    		  success: function(response){   
+    		  try{  	 
+    			 //  var result = jQuery.parseJSON(data);
+    			   if (response.success)
+    	           { 	
+    				 $('#MasBranchformColap').html(response.html);
+                     $(window).scrollTop(0);
+    	           } else
+    	           { 
+    	               SetWarningMessageBox('warning', response.msg);
+    	           }
+    		  StopInsideLoading();
+    		  }catch(e) {  
+    			  SetWarningMessageBox('warning', e);
+    			  StopInsideLoading();
+    		  }  
+    		  },
+    		  error: function(){      
+    			  SetWarningMessageBox('warning', 'Error while request..');
+    			  StopInsideLoading();
+    		  }
+    		 });
+    }
+
+      
+    function editBranch($btn){  
+    	$reqestId =  $btn.val(); 
+    	var url = '<?php echo base_url();?>index.php/data_controller/EditBranch';
+    	StartInsideLoading();
+    	$.ajax({
+    		  type: "post",
+    		  url: url,
+    		  cache: false,    
+    		  data: {reqId:$reqestId},
+    		  dataType: 'json',
+    		  success: function(response){   
+    		  try{  	 
+    			   if (response.success)
+    	           { 	
+    				 $('#MasBranchformColap').html(response.html);
+                     $(window).scrollTop(0);
+    	           } else
+    	           { 
+    	               SetWarningMessageBox('warning', response.msg);
+    	           }
+    		 StopInsideLoading();
+    		  }catch(e) {  
+    			  SetWarningMessageBox('warning', e);
+    			  StopInsideLoading();
+    		  }  
+    		  },
+    		  error: function(){      
+    			  SetWarningMessageBox('warning', 'Error while request..');
+    			  StopInsideLoading();
+    		  }
+    		 });
+    } 
+   
+    function updateBranch(){  
+    	if ($('#branch_name').val().trim() == '') { 
+            SetWarningMessageBox('warning', 'Branch name is mandatory !');
+            $('#branch_name').focus();
+            return;
+        }
+			if ($('#branch_code').val().trim() == '') { 
+            SetWarningMessageBox('warning', 'Branch code is mandatory !');
+            $('#branch_code').focus();
+            return;
+        }
+			if ($('#branch_address').val().trim() == '') { 
+            SetWarningMessageBox('warning', 'Branch address is mandatory !');
+            $('#branch_address').focus();
+            return;
+        }
+       
+
+        var formData = $('form#BranchFormUpdate').serializeObject();
+        var dataString = JSON.stringify(formData);
+        var url = '<?php echo base_url();?>index.php/data_controller/updateBranch';
+        StartInsideLoading();
+		 $.ajax({
+		  type: "post",
+		  url: url,
+		  cache: false,    
+		  data: dataString,
+		  dataType: 'json',
+		  success: function(response){   
+		  try{  	
+			   if (response.success)
+	           { 
+				   SetSucessMessageBox('Success', response.msg);
+				   $('#MasBranchformColap').empty(); 
+				   loadBranch();
+				   $('#branch').DataTable();
+	           } else
+	           { 
+	               SetWarningMessageBox('warning', response.msg);
+	           }
+		  StopInsideLoading();
+		  }catch(e) {  
+			  SetWarningMessageBox('warning', e);
+			  StopInsideLoading();
+		  }  
+		  },
+		  error: function(){      
+			  SetWarningMessageBox('warning', 'Error while request..');
+			  StopInsideLoading();
+		  }
+		 });
+	}
+    function deleteBranch(){  
+
+       // Checking all category data are deleted
+    	if (!$( ".checkbox" ).length) {
+    		SetWarningMessageBox('warning', 'No Item left  to Delete !!!'); 
+    		return;
+    	}
+    	
+    	var selected_value = []; // initialize empty array 
+    	if ($('.checkbox:checked').length == 0 )
+        {
+    		SetWarningMessageBox('warning', 'Please select Item to Delete !!!');
+    		return;
+	    } else {
+	    	$(".checkbox:checked").each(function(){
+	              selected_value.push($(this).val());
+	          });
+	    }	
+    	var url = '<?php echo base_url();?>index.php/data_controller/RemoveBranch';
+    	var dataString = JSON.stringify(selected_value);
+    swal({
+      title: "Are you sure?",
+      //text: "You will not be able to recover this imaginary file!",
+      //type: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, Delete it!",
+      cancelButtonText: "No, cancel plx!",
+      closeOnConfirm: true,
+      closeOnCancel: true
+      }, function(isConfirm) {
+      if (isConfirm) {
+      StartInsideLoading();  
+        	$.ajax({
+      		  type: "post",
+      		  url: url,
+      		  cache: false,    
+      		  data: {dataArr:dataString},
+      		  dataType: 'json',
+      		  success: function(response){   
+      		  try{  	
+      			   if (response.success)
+      	           { 
+      				   SetSucessMessageBox('Success', response.msg);
+      				   loadBranch();
+      				   $('#branch').DataTable();
+      	           } else
+      	           { 
+      	               SetWarningMessageBox('warning', response.msg);
+      	               //StopInsideLoading();
+      	           }
+      		  StopInsideLoading();
+      		  }catch(e) {  
+      			  SetWarningMessageBox('warning', e);
+      			  StopInsideLoading();
+      		  }  
+      		  },
+      		  error: function(){      
+      			  SetWarningMessageBox('warning', 'Error while request..');
+      			  StopInsideLoading();
+      		  }
+      		 });
+      }
+      }); 
+    } 
+</script>
+    
+       </body>
+</html>
