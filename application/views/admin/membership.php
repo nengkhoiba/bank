702ec9db367102bdb0da588b555d2f13b65ffc1a
WilliamThoudam <32706232+WilliamThoudam@users.noreply.github.com>
2018-09-02 14:07:23 +0530

    g

--- application/views/admin/membership.php
+++ application/views/admin/membership.php
@@ -5,7 +5,7 @@
         <div>
         <ul class="app-breadcrumb breadcrumb">
           <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-          <li class="breadcrumb-item">Membership Form</li>
+          <li class="breadcrumb-item">Membership Registration</li>
         </ul>
 		</div>
 		<p class="bs-component">	
