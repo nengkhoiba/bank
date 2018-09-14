b5c8be5dbd7a322643ace5f3e74c4964290d0044
WilliamThoudam <32706232+WilliamThoudam@users.noreply.github.com>
2018-09-14 17:00:51 +0530

    wwww

--- application/views/datafragment/dataTable/Emp_table.php
+++ application/views/datafragment/dataTable/Emp_table.php
@@ -34,7 +34,7 @@
                 <input class="checkbox" type="checkbox" value="<?php echo $emp_data['ID']?>"><span class="label-text"></span>
               	</label>
             	</div>
-            	<button onclick="editEmp($(this))" value="<?php echo $emp_data['ID']?>"class="btn btn-primary w2wbutton" style="" type="button"><i style ="font-size: 12px; margin-right: 0px;" class="fa fa-lg fa-fw fa-pencil"></i></button>
+            	<button onclick="addEmpform($(this))" value="<?php echo $emp_data['ID']?>"class="btn btn-primary w2wbutton" style="" type="button"><i style ="font-size: 12px; margin-right: 0px;" class="fa fa-lg fa-fw fa-pencil"></i></button>
 				</td>
 				</tr>
         <?php $count ++;} ?>
