bba7a270bd88a11e3fe4b99deec8f8d41c02d1e1
WilliamThoudam <32706232+WilliamThoudam@users.noreply.github.com>
2018-09-16 13:38:09 +0530

    update for veiw profile

--- application/views/admin/customerprofile.php
+++ application/views/admin/customerprofile.php
@@ -20,8 +20,68 @@
               width: 36px;"><span aria-hidden="true">×</span></button>
             </div>
             <div class="tile-body">
-           
-		    </div>
+              <div class="row invoice-info">
+                <div class="col-6">
+                <b>Customer ID : <span id="customer_ID"></span><br><br></b>
+                    <b><address>Name : </b><span id="customer_Name"></span><br>
+                    <b>Date of Birth : </b><span id="customer_dob"></span><br>
+                    <b>Gender : </b><span id="customer_gender"></span><br>
+                    <b>Aadhaar No. : </b><span id="customer_aadhaar"></span><br>
+                    <b>Husband/Father Name :</b><span id="customer_husband"></span><br>
+                   <b> Permanent Address : </b><span id="customer_address"></span></address>
+                </div>
+                <div class="col-6">
+                <b>Order Detail </b><br><br>
+                  <b>Order ID : </b><?php echo 'Qty';?><br>
+                  <b>Date : </b><?php echo 'Qty'; ?><br>
+                  <b>Order Status : </b><span style="color:blue;"><?php echo 'Qty';?></span><br> 
+                  <b>Returned Reason : </b><span style="color:black;"><?php echo 'Qty';?></span><br> 
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12 table-responsive">
+                  <table class="table table-striped">
+                    <thead>
+                      <tr>
+                        <th>Qty</th>
+                        <th>Item</th>
+                        <th>Price</th>
+                        <th>Shipping Charge</th>
+                        <th>Subtotal ( <img src="http://i.stack.imgur.com/nGbfO.png" width="8" height="10"> )</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td><?php echo 'Qty';?></td>
+                        <td><?php echo 'Item';?></td>
+                        <td><?php echo 'Price';?></td>
+                        <td><?php echo 'Shipping Charge';?></td>
+                        <td><?php echo 'Subtotal';?></td>
+                      </tr>
+                      <tr>
+                        <td><?php echo 'Qty';?></td>
+                        <td><?php echo 'Item';?></td>
+                        <td><?php echo 'Price';?></td>
+                        <td><?php echo 'Shipping Charge';?></td>
+                        <td><?php echo 'Subtotal';?></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+		<hr> 
+    <form class="row" id="OrdersFormsUpdate">
+                    <div class="form-group col-md-8 align-self-end" id='Btn_area_verify' style="visibility: visible; display:block;">
+                      <button onclick="updateOrders(2)" class="btn btn-success" type="button"><i class="fa fa-fw fa-lg fa-check-circle"></i>Verified</button>
+                      &nbsp;&nbsp;&nbsp;
+                      <button onclick="updateOrders(6)" class="btn btn-danger" type="button"><i class="fa fa-fw fa-lg fa-times-circle"></i>Reject Order</button>
+                    </div>
+                    <div class="col-4 text-right">
+                      <button class="btn btn-primary" onclick="printDiv('printarea')"><i class="fa fa-print"></i> Print</button>
+                      <a class="btn btn-primary icon-btn" href="<?php echo site_url() . 'Data_controller/save_pdf_download?req=' .base64_encode('ID'); ?>" target="_blank"><i class="fa fa-file"></i>PDF</a>
+                  </div>        
+		</form>
+		           </div>
           </div>
         </div> 
       </div>
@@ -95,8 +155,15 @@
     		  success: function(response){   
     		  try{  	 
     			   if (response.success)
-    	           { 	
-    				 $('#postType').val(response.json[0].ID);
+    	           {
+    				 $('#customer_ID').html(response.json[0].ID);
+    				 $('#customer_Name').html(response.json[0].name);
+    				 $('#customer_dob').html(response.json[0].dob);
+    				 $('#customer_gender').html(response.json[0].sex);
+    				 $('#customer_aadhaar').html(response.json[0].aadhaar_no);
+    				 $('#customer_husband').html(response.json[0].husband_name);
+    				 $('#customer_address').html(response.json[0].parmanent_address);
+    				 
                      $(window).scrollTop(0);
                      $('#formContainer').show();
     	           } else
