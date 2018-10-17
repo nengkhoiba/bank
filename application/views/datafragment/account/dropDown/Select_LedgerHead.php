5caa0c4f8da310a47b6dee3f19fc3eeab718c643
WilliamThoudam <32706232+WilliamThoudam@users.noreply.github.com>
2018-10-17 19:24:24 +0530

    Update application/views/datafragment/account/dropDown/Select_LedgerHead.php

--- application/views/datafragment/account/dropDown/Select_LedgerHead.php
+++ application/views/datafragment/account/dropDown/Select_LedgerHead.php
@@ -1,5 +1,4 @@
 	<option class="form-control" value="">- Select -</option>
-	<option class="form-control" value="Pri">Primary</option>
 <?php  foreach ($result as $list)   { ?>
   	<option class="form-control" value="<?php echo $list['ID'];?>"><?php echo $list['Ledger'];?></option>
 <?php  } ?> 
